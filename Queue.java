dfec7190d97fe0ebc805680089622bd863c5d242
Horacio H <hernandezh4@hotmail.com>
2024-02-27 19:15:11 -0800

    Finish Queue from scratch

---
+++ Queue.java
@@ -0,0 +1,18 @@
+public class Queue {
+    private class Node{
+        Node next;
+        int elem;
+
+        public Node(int elem){
+            this.elem = elem;
+            this.next = null;
+        }
+    }
+
+    public void offer(int elem){
+        Node add = new Node(elem);
+
+
+    }
+
+}
